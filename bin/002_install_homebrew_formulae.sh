e7c17f1e94caa5cf6264c4b214b3cdaf1452bf16
Nicolas Maurice <nmvalera@mbp-de-nicolas.home>
2023-11-10 12:41:26 +0100

    feat(formula): install tools for software development and system administration.
    - 1password-cli
    - asciinema
    - bash
    - bash-completion
    - coreutils
    - docker
    - docker-compose
    - git
    - go
    - gpg
    - htop
    - jq
    - mas
    - openssh
    - openssl
    - postgresql
    - python
    - terraform
    - vim
    - wget
    - zsh
    - zsh-git-prompt
    - zsh-history-substring-search
    - zsh-lovers
    - zsh-navigation-tools
    - zsh-syntax-highlighting
    - zshdb

---
+++ bin/002_install_homebrew_formulae.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+# Install Homebrew Formulae
+install_formula() {
+  package=$1
+  if test ! $(brew list | grep -w "$package"); then
+    echo "Installing $package..."
+    brew install $package
+  else
+    echo "$package already installed"
+  fi
+}
+
+install_formula "1password-cli"
+install_formula "asciinema"
+install_formula "bash"
+install_formula "bash-completion"
+install_formula "coreutils"
+install_formula "docker"
+install_formula "docker-compose"
+install_formula "git"
+install_formula "go"
+install_formula "gpg"
+install_formula "htop"
+install_formula "jq"
+install_formula "mas"
+install_formula "openssh"
+install_formula "openssl"
+install_formula "postgresql"
+install_formula "python"
+install_formula "terraform"
+install_formula "vim"
+install_formula "wget"
+install_formula "zsh"
+install_formula "zsh-git-prompt"
+install_formula "zsh-history-substring-search"
+install_formula "zsh-lovers"
+install_formula "zsh-navigation-tools"
+install_formula "zsh-syntax-highlighting"
+install_formula "zshdb"
